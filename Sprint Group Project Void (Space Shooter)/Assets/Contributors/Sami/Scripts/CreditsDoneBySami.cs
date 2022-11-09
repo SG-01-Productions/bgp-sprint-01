771ff26ef3a0d4683d0b55cf66a52571d116aebb
Rykmentti <Razorworks991@hotmail.com>
2022-11-09 17:56:45 +0200

    random commit

---
+++ Sprint Group Project Void (Space Shooter)/Assets/Contributors/Sami/Scripts/CreditsDoneBySami.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    private TMP_Text creditsText;
+    [SerializeField] int credits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        credits = 5;
+        creditsText = gameObject.GetComponent<TMP_Text>();
+        creditsText.text = "Test" + credits;
+    }
+}
