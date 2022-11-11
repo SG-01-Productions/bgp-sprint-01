dbdf6310c37d9b4603778a0a2cfd93ec3f424aa6
Rykmentti <Razorworks991@hotmail.com>
2022-11-11 12:44:29 +0200

    update commit
    forgot comment on other script

--- Sprint Group Project Void (Space Shooter)/Assets/Contributors/Sami/Scripts/LeftTurretScript.cs
+++ Sprint Group Project Void (Space Shooter)/Assets/Contributors/Sami/Scripts/LeftTurretScript.cs
@@ -26,7 +26,7 @@ public class LeftTurretScript : MonoBehaviour
             Vector2 Point_2 = new Vector2(playerPosX, playerPosY);
             float rotation = Mathf.Atan2(Point_2.y - Point_1.y, Point_2.x - Point_1.x) * Mathf.Rad2Deg;
             Quaternion compensationAngle = GetComponentInParent<CharControl>().transform.rotation;
-            Vector3 eulerCompensationAngle = compensationAngle.eulerAngles;
+            Vector3 eulerCompensationAngle = compensationAngle.eulerAngles; //Compensation for the ship's angle to the turret rotation.
             Vector3 turretStartRotation = new Vector3(0f, 0f, rotation - eulerCompensationAngle.x);
             Quaternion quaternion = Quaternion.Euler(turretStartRotation);
 
