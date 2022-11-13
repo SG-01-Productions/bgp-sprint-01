a55f43fb5ecbf2900b0e0075517fb6093d21963d
Jeremy LeBlanc <leblancjs88@gmail.com>
2022-11-13 22:32:50 +0200

    Updated functionality for resource gains from static asteroid field objects.

--- Sprint Group Project Void (Space Shooter)/Assets/Contributors/Sami/Scripts/HealthManager.cs
+++ Sprint Group Project Void (Space Shooter)/Assets/Contributors/Sami/Scripts/HealthManager.cs
@@ -1,15 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
     private float asteroidFullHealth;
     public float asteroidHealth;
+    public int bigAsteroidHealth;
+    public int bigAsteroidCreditValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bigAsteroidHealth = Convert.ToInt32(asteroidHealth);
+        bigAsteroidCreditValue = bigAsteroidHealth;
     }
 
     // Update is called once per frame
@@ -42,8 +44,10 @@ public class HealthManager : MonoBehaviour
 
     public void DestroyAsteroid()
     {
+        int incomingCredits = bigAsteroidHealth;
         AsteroidFieldManager.Instance.asteroidDestroyed(transform.position, asteroidFullHealth * 0.33f);
-
+        GetComponent<ResourceManager>().AsteroidFieldCredits(incomingCredits);
         Destroy(gameObject);
     }
+
 }
